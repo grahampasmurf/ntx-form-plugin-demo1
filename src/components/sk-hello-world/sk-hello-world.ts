1f89c8dc0a95f4981aae70a04cc5591f7ce63b6b
grahampasmurf <36420136+grahampasmurf@users.noreply.github.com>
2024-06-12 13:16:20 -0500

    new typo

--- src/components/sk-hello-world/sk-hello-world.ts
+++ src/components/sk-hello-world/sk-hello-world.ts
@@ -21,7 +21,7 @@ export class PageHighlight extends LitElement {
 		return html`
 			${addBootstrap()}
 			<div>
-				<h1>Hello w0rld!</h1>
+				<h1>Hello world!</h1>
 				<span style="color: ${this.color}">${this.message}</span>
 			</div>
 		`;
